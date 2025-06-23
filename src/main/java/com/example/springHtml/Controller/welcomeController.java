c0d3f3fcfc10fcbb61a77a6fed6b4c9c748aee90
BhargavWolean <Bhargav1s2s3s@gmail.com>
2025-06-23 14:00:24 +0530

    issue with image fateching from db sloved and name becomes null after new issue was sloved

---
+++ src/main/java/com/example/springHtml/Controller/welcomeController.java
@@ -0,0 +1,106 @@
+package com.example.springHtml.Controller;
+
+import com.example.springHtml.Model.UserModel;
+import com.example.springHtml.Repository.ImageRepo;
+import com.example.springHtml.Repository.UserRepo;
+import com.example.springHtml.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.List;
+
+
+@Controller
+@RequestMapping("/main")
+public class welcomeController {
+
+    @Autowired
+    private UserRepo usRep;
+
+    @Autowired
+    UserService service;
+
+    @Autowired
+    private ImageRepo imgRepo;
+
+    private String userfname;
+
+
+    @GetMapping("/home")
+    public String Greet() {
+        return "welcome";
+    }
+
+
+
+
+    @PostMapping("/home")
+    public ModelAndView SignIn(@RequestParam String email, @RequestParam String password){
+        String res = service.checkMailAndPassword(email, password);
+        userfname = service.getAllByEmail(email).getFname().toString();
+        System.out.println(userfname);
+
+        if(!res.equals("failed")) {
+            System.out.println("Content primited");
+            return new ModelAndView("redirect:/main/access");
+        }
+
+        System.out.println("Content not Primited");
+        return new ModelAndView("redirect:/main/signup");
+    }
+
+    @GetMapping("/access")
+    public String AccessContentPage(Model model) {
+        model.addAttribute("fname",userfname);
+        List<String> urls = imgRepo.getUrls();
+        model.addAttribute("imageUrls",urls);
+        return "ContentAccess";
+    }
+
+    @PostMapping("/access")
+    public String uploadImage(@RequestParam String email ,@RequestParam String url, Model model) {
+        String res = service.addUrl(email, url);
+
+        if (res.equals("Uploaded")) {
+            model.addAttribute("fname",userfname);
+
+            List<String> urls = imgRepo.getUrls();
+            System.out.println("Controller image upload working");
+            model.addAttribute("imageUrls", urls);
+        }
+        System.out.println("Controller image upload not working");
+        return "ContentAccess";
+    }
+
+
+
+
+    @GetMapping("/signup")
+    private String SignUpPage() {
+        System.out.println("signUP test");
+        return "signupPage";
+    }
+
+    @PostMapping("/signup")
+    public String SignUpData(@RequestParam String fname,
+                             @RequestParam String lname,
+                             @RequestParam String email,
+                             @RequestParam String password,
+                             Model model) {
+        UserModel user = new UserModel(fname,lname,email,password);
+        usRep.save(user);
+        model.addAttribute("userFirstName",fname);
+        System.out.println("signUP adding test");
+        return "welcomeGreetsPage";
+
+    }
+}
+
